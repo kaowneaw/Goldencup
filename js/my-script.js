3bc781517a20c31dc6366004d923c12576d9649c
Kaowneaw-PC\Kaowneaw <th.panya.bas@gmail.com>
2017-03-26 20:39:33 +0700

    init

---
+++ js/my-script.js
@@ -0,0 +1,13 @@
+var carouselDefault = new PureJSCarousel({
+    carousel: '#menu',
+    slide: '.slide',
+    oneByOne: true
+});
+
+$('#lightSlider').lightSlider({
+        gallery: true,
+        item: 1,
+        loop: true,
+        slideMargin: 0,
+        thumbItem: 4
+});
